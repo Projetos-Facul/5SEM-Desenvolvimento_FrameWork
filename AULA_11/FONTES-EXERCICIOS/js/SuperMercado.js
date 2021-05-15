7a06c62a97342f436d76b02249f6581f659ffc7a
Vinicius Pinto Guedes <viniciusguedes.1007@gmail.com>
2021-05-14 21:57:00 -0300

    Lista 11

--- AULA_11/FONTES-EXERCICIOS/js/SuperMercado.js
+++ AULA_11/FONTES-EXERCICIOS/js/SuperMercado.js
@@ -9,30 +9,33 @@ $(document).ready(function()
 
         $('input[type="text"]').focus(function()
             {
-                console.log("Focus");
                 Focus(this);
             }
         );
 
         $('input[type="text"]').blur(function()
             {
-                console.log("Blur");
                 Blur(this);
             }
         );
 
         $('#btnCalc').click(function()
             {
-                Calcular ();
+                Calcular ("Geral");
             }
         );
 
-        $('input[type="text"]').bind('keyup paste', function()
-            {
-                console.log("OnlyNumber");
-                OnlyNumeber(this);
-            }
-        );
+        $("input[type='text']").keypress(function (e) {
+            if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+               $("#" + this.id + "Mensagem")
+                    .html("Digite somente numeros")
+                    .css("color", "red")
+                    .fadeIn(100)
+                    .fadeOut(3500);
+                
+                return false;
+           }
+        });
     }
 );
 
@@ -48,31 +51,30 @@ function Blur(element)
     $("#" + element.id).css("color", "black");
 }
 
-function OnlyNumeber(element)
+function Calcular (form)
 {
-    element.value = element.value.replace(/[^0-9]/g, '');
-}
-
-function Calcular ()
-{
-    var Seco1       = document.getElementById("txtQtdSeco1");
-    var Seco2       = document.getElementById("txtQtdSeco2");
-    var Molhado1    = document.getElementById("txtQtdMolhado1");
-    var Molhado2    = document.getElementById("txtQtdMolhado2");
-    var Fragil1     = document.getElementById("txtQtdFragil1");
-    var Fragil2     = document.getElementById("txtQtdFragil2");
-
-    if( Seco1.value     == ""   |   Seco2.value     == ""   | 
-        Molhado1.value  == ""   |   Molhado2.value  == ""   |
-        Fragil1.value   == ""   |   Fragil2.value   == "")
+    var formulario = document.getElementById(form).elements;
+    var cont = 0;
+    alert(formulario.length);
+    for(i=0; i < formulario.length; i++)
     {
-        alert("Preencha todos os campos com no minino 0");
+        var input = formulario.item(i);
+        if(input.value == "" )
+        {
+            $("#" + input.id).css("background-color", "red");
+            cont=1;
+        }
+    }
+
+    if (cont == 1)
+    {
+        alert("Preencha todos os campos com no minimo 0");
     }
     else
-    {
-        var resSeco     = Math.ceil(parseInt(Seco1.value)       / parseInt(Seco2.value));
-        var resMolhado  = Math.ceil(parseInt(Molhado1.value)    / parseInt(Molhado2.value));
-        var resFragil   = Math.ceil(parseInt(Fragil1.value)     / parseInt(Fragil2.value));
+    {   
+        var resSeco     = Math.ceil(parseInt(formulario.item(0).value)    / parseInt(formulario.item(1).value));
+        var resMolhado  = Math.ceil(parseInt(formulario.item(2).value)    / parseInt(formulario.item(3).value));
+        var resFragil   = Math.ceil(parseInt(formulario.item(4).value)    / parseInt(formulario.item(5).value));
 
         alert ( "Para esta compra deve usar: " + (resSeco+resMolhado+resFragil) + " Sacolas"
                 +"\n Sendo: \n        "
